597a5b36d6515ca00c7a85e8e71ad955bf2dfba1
fdelys <frandel932@hotmail.com>
2020-10-07 15:32:41 +0200

    début dev, usage groupby instead of N variables

---
+++ calcul_moyenne.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 15:31:02 2020
+
+@author: formateur
+"""
+
+import datetime
+import pandas as pd
+
+url = "https://opendata.arcgis.com/datasets/6f64bbd4f94c425791c2ec7eee33bb71_0.csv"
+df = pd.read_csv(url)
+
+var = df.loc[:, ["val_no2", "date_ech", "lib_zone"]][df.lib_zone == "BLOIS"][df.date_ech >= "2018/01/01 00:00:00+00"][df.date_ech < "2018/02/01 00:00:00+00"].sort_values(by= ["date_ech"])
+
+test = df.loc[:,["val_so2","val_03","val_pm25","val_pm10","date_ech","lib_zone"]].groupby([df.lib_zone == "BLOIS"]).mean()
+
+print(test)
