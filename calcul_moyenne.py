bc9c7f4fa3c3c110ecd7a8c42fe98282eb3bfc77
fdelys <frandel932@hotmail.com>
2020-10-07 16:21:07 +0200

    evolution vers DatetimeIndex de pandas, pas biblio DateTime

--- calcul_moyenne.py
+++ calcul_moyenne.py
@@ -8,12 +8,24 @@ Created on Wed Oct  7 15:31:02 2020
 
 import datetime
 import pandas as pd
+import folium
 
 url = "https://opendata.arcgis.com/datasets/6f64bbd4f94c425791c2ec7eee33bb71_0.csv"
 df = pd.read_csv(url)
 
+
+jour = pd.DatetimeIndex(df["date_ech"]).day
+mois = pd.DatetimeIndex(df["date_ech"]).month
+an = pd.DatetimeIndex(df["date_ech"]).year
+
+
+print(jour)
+'''
 var = df.loc[:, ["val_no2", "date_ech", "lib_zone"]][df.lib_zone == "BLOIS"][df.date_ech >= "2018/01/01 00:00:00+00"][df.date_ech < "2018/02/01 00:00:00+00"].sort_values(by= ["date_ech"])
 
-test = df.loc[:,["val_so2","val_03","val_pm25","val_pm10","date_ech","lib_zone"]].groupby([df.lib_zone == "BLOIS"]).mean()
+test = df.loc(:,["val_no2","val_so2","val_o3","val_pm25","val_pm10","date_ech",[df.lib_zone == "BLOIS"]]).groupby(["BLOIS"]).mean()
 
 print(test)
+
+var = df.loc[:, ["date_ech"]]
+'''
