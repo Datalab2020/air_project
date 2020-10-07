cdfb584f29df18dd4fcb661608a9ae9ed79bc7b8
xavierboue <xavierboue37@gmail.com>
2020-10-07 16:21:49 +0200

    second

--- projet1.py
+++ projet1.py
@@ -6,4 +6,19 @@ Created on Wed Oct  7 14:48:22 2020
 @author: formateur
 """
 
-print("bonour")
+" Projet qualité de l'air en Région Centre "
+
+
+import pandas as pd
+import folium 
+
+url = "https://opendata.arcgis.com/datasets/6f64bbd4f94c425791c2ec7eee33bb71_0.csv"
+df = pd.read_csv(url)
+
+
+print(df)
+
+
+
+
+
