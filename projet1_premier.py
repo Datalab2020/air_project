d6ed6d7757026b0f8bc941aae1b980dddf8e6b99
fdelys <frandel932@hotmail.com>
2020-10-08 14:27:52 +0200

    Réu

--- projet1_premier.py
+++ projet1_premier.py
@@ -8,7 +8,7 @@ df = pd.read_csv(url)
 #print(df.columns)
 #print(df.loc[0:50, ["Y", "X", "valeur", "date_ech", "lib_zone"]].sort_values(by= ["lib_zone", "date_ech"], na_position= "last"))
 #print(df.groupby("lib_zone").mean().loc[:, ["valeur"]])
-#print(df.groupby("lib_zone").mean().reset_index().loc[:, ["valeur", "lib_zone"]])
+print(df.groupby("lib_zone").mean().reset_index().loc[:, ["valeur", "lib_zone"]])
 #print(df.loc[0:50, ["Y", "X", "valeur", "date_ech", "lib_zone"]].df[df["lib_zone"]=="BLOIS"])
 #print(df[df["lib_zone"]=="BLOIS"])
 
