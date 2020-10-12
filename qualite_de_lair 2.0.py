956746bcb06b162333956d965a38c25072f57418
Cesarp27 <cesardeveloppeurdata2020@gmail.com>
2020-10-12 11:20:23 +0200

    modification pour faciliter les graphiques

--- qualite_de_lair 2.0.py
+++ qualite_de_lair 2.0.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct  8 15:04:47 2020
+Created on Sat Oct 10 20:52:13 2020
 
 @author: formateur
 """
@@ -10,6 +10,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
+import pandas_profiling
+
 
 url = "https://opendata.arcgis.com/datasets/6f64bbd4f94c425791c2ec7eee33bb71_0.csv"
 df = pd.read_csv(url)
@@ -32,22 +34,10 @@ aggregation = group_villes2.agg({'val_no2' : 'mean',
 
 aggregation_index = aggregation.reset_index().sort_values(by=['date_ech','lib_zone'])
 
-plt.figure(figsize=(20, 16))
-sns.relplot(x="date_ech", y="valeur", kind="line", hue="lib_zone", col="lib_zone", col_wrap=3,
-            data=aggregation_index.query("date_ech >= '2018/07/01 00:00:00+00' & date_ech < '2019/01/01 00:00:00+00'"))
-
-
-"""
 #avec cette ligne je crée une variable avec le df de chaque ville  
 for ville in aggregation_index['lib_zone']:
         globals()['Ville_%s' % ville] = aggregation_index.loc[:, ][aggregation_index.lib_zone == ville].sort_values(by=['date_ech']).reset_index()
 
-#print(Ville_BLOIS)
 
-
-Bl = aggregation_index.loc[:, ][aggregation_index.lib_zone == 'BLOIS'][aggregation_index.an > 2018].sort_values(by=['an', 'mois']).reset_index()
-#Bl[['val_no2','val_so2','val_o3','val_pm10']].plot.hist(alpha=0.3)
-#Bl[['val_no2','val_so2','val_o3','val_pm10']].plot.kde()
-"""   
-        
-
+profile = pandas_profiling.ProfileReport(Ville_TOURS)
+profile.to_file("Tours.html")
